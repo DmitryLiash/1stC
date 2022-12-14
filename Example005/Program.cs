dfb169b8be50afa9c5309ed26f46c3a30c9352ed
DmitryL <dimak.pax@gmail.com>
2022-12-14 20:03:18 +0600

    ssss

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.WriteLine("Ура Маша !1!У1");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
